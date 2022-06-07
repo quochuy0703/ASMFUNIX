a01f0bf89a97f29534013f519041c5910cd34edd
Huy Mai <quochuy0703@gmail.com>
2022-06-07 17:31:08 +0700

    ASM3_commit_9

--- src/components/SearchStaff.js
+++ src/components/SearchStaff.js
@@ -4,20 +4,20 @@ import { Button, InputGroup, Input } from "reactstrap";
 class SearchStaff extends Component {
   constructor(props) {
     super(props);
-    this.searchInput = React.createRef();
+    // this.searchInput = React.createRef();
     this.handleSearch = this.handleSearch.bind(this);
   }
 
   handleSearch() {
-    this.props.onSearch(this.searchInput.current.value);
-    this.searchInput.current.value = "";
+    this.props.onSearch(this.searchInput.value);
+    this.searchInput.value = "";
   }
   render() {
     return (
       <div className="col-md-4 col-sm-6 col-xs-12">
         <InputGroup>
           <Input
-            innerRef={this.searchInput}
+            innerRef={(input) => (this.searchInput = input)}
             placeholder="Nhập tên để tìm kiếm, để trống về danh sách ban đầu"
           />
           <Button onClick={this.handleSearch}>Search</Button>
